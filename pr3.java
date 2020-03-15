60f3e023d89bc2e0cba01e07f738d5ff3029cf29
MinJae <guswlfahs123@naver.com>
2020-03-15 18:33:06 +0900

    pr3.java 참조하는 위치가 복사. ==연산으로 참조위치가 같은지 판단

---
+++ pr3.java
@@ -0,0 +1,43 @@
+package ex1.io;
+import java.util.Scanner;
+class Day
+{
+	private int year;
+	private int month;
+	private int date;
+	
+	Day(int year,int month,int date)//생성자
+	{
+		this.year=year; this.month=month; this.date=date;
+	}
+	int getYear() {return year;}
+	int getMonth() {return month;}
+	int getDate() {return date;}
+	void set(int year,int month, int date)//재설정하기 
+	{
+		this.year=year; this.month=month; this.date=date;
+	}
+}
+public class Program {
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		Day day1=new Day(1998,5,21);
+		System.out.println(day1.getYear());
+		System.out.println(day1.getMonth());
+		System.out.println(day1.getDate());
+		Day day2=day1; //day1을 가리키는 참조가 day2에 복사. *참조가 복사됨*
+		day2.set(2020,3,15);
+		System.out.println(day2.getYear());
+		System.out.println(day2.getMonth());
+		System.out.println(day2.getDate());
+		if(day1==day2)
+			System.out.println("day1과 day2는같습니다");
+		else
+			System.out.println("day1과 day2는 다릅니다");
+	}
+
+}
+
+
